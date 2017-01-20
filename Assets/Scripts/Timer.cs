070936d03a0a03c3cd508540a466feb1a27dcf06
Darien Livermore <N3010119@live.tees.ac.uk>
2017-01-20 21:15:47 +0000

    Time && UI Handler

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    [SerializeField, Range(30.0f, 180.0f)]
+    private float m_initialTime = 120.0f;
+
+    [SerializeField]
+    private Text m_clock;
+
+    private bool m_active = false;
+    private float m_time;
+
+    public bool isActive { get { return m_active; } }
+
+    public void StartTimer()
+    {
+        m_time = m_initialTime;
+        m_active = true;
+    }
+
+    private void TimeLoop()
+    {
+        if (m_active)
+        {
+            m_time -= Time.deltaTime;
+            if(m_time < 0.0f)
+            {
+                m_time = 0.0f;
+                m_active = false;
+            }
+            m_clock.text = string.Format("{0}:{1:00}:{2:00}", (int)m_time / 60, (int)m_time % 60, (m_time - (int)m_time) * 99);
+        }
+    }
+
+	void Update ()
+    {
+        TimeLoop();
+        if(Input.anyKeyDown)
+        {
+            StartTimer();
+        }
+	}
+}
---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIHandler : MonoBehaviour {
+
+    [System.Serializable]
+    private class TextArray
+    {
+        public Text[] text;
+    }
+
+    [SerializeField, Range(1.0f, 5.0f)]
+    private float m_fadeRate = 2.0f;
+    [SerializeField]
+    private TextArray[] m_text;
+
+    private bool m_fade = false;
+    
+	void Start () {
+        foreach(TextArray a in m_text)
+        {
+            foreach(Text t in a.text)
+            {
+                Color c = t.color;
+                c.a = 0.0f;
+                t.color = c;
+            }
+        }
+	}
+
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            Fade();
+        }
+    }
+
+    public void Fade(uint collection = 0, bool fadeIn = true)
+    {
+        m_fade = true;
+        StartCoroutine(FadeLoop(collection, fadeIn));
+        StartCoroutine(FadeCancel());
+    }
+
+    private IEnumerator FadeLoop(uint collection, bool fadeIn)
+    {
+        yield return new WaitWhile(() => {
+            foreach (Text t in m_text[collection].text)
+            {
+                Color c = t.color;
+                c.a += (fadeIn ? Time.deltaTime : -Time.deltaTime) / m_fadeRate;
+                t.color = c;
+            }
+            return m_fade;
+        });
+    }
+
+    private IEnumerator FadeCancel()
+    {
+        yield return new WaitForSeconds(m_fadeRate);
+        m_fade = false;
+    }
+
+}
