b19fbafec85d96aa1f9bcf6b0ef163caea763882
Thor Bunting <thor.bunting@live.co.uk>
2017-01-21 01:12:01 +0000

    The Tentacle Returns

---
+++ Assets/Scripts/TentacleControls.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TentacleControls : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tentacleTop;
+    private float forceMultiplier = 20;
+
+    void Update()
+    {
+        float vert = Input.GetAxis("Vertical") * forceMultiplier;
+        float hori = Input.GetAxis("Horizontal") * forceMultiplier;
+
+        Debug.Log("Force Added: " + new Vector3(vert, 0f, hori));
+
+        tentacleTop.GetComponent<Rigidbody>().AddForce(vert, 0f, hori, ForceMode.Force);
+    }
+}
