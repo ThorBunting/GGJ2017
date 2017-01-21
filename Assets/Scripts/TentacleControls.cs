b41329efc981738dad2c5b5038c73eeb3c465245
Darien Livermore <N3010119@live.tees.ac.uk>
2017-01-21 03:29:27 +0000

    Improved input.

--- Assets/Scripts/TentacleControls.cs
+++ Assets/Scripts/TentacleControls.cs
@@ -9,19 +9,28 @@ public class TentacleControls : MonoBehaviour
     [SerializeField]
     private int m_player = 0;
 
+    private string verticalAxis;
+    private string horizontalAxis;
+
     private float forceMultiplier = 20;
 
     public int Player { get { return m_player; } }
 
+    void Start()
+    {
+        verticalAxis = "Vertical" + m_player.ToString();
+        horizontalAxis = "Horizontal" + m_player.ToString();
+    }
+
     void Update()
     {
-        float vert = Input.GetAxis("Vertical") * forceMultiplier;
-        float hori = Input.GetAxis("Horizontal") * forceMultiplier;
+        float vert = Input.GetAxis(verticalAxis) * forceMultiplier;
+        float hori = Input.GetAxis(horizontalAxis) * forceMultiplier;
 
         //if (hori >= 0)
 
             //Debug.Log("Force Added: " + new Vector3(vert, 0f, hori));
 
-        tentacleTop.GetComponent<Rigidbody>().AddForce(vert, 0f, hori, ForceMode.Force);
+        tentacleTop.GetComponent<Rigidbody>().AddForce(hori, Mathf.Min(0.0f, vert), Mathf.Max(0.0f, -vert), ForceMode.Force);
     }
 }
