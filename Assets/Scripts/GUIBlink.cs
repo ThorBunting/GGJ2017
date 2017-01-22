9cdfb8087f99c0510574825494ef7c41dc72d411
Thor Bunting <thor.bunting@live.co.uk>
2017-01-22 13:32:37 +0000

    Blink Anim

---
+++ Assets/Scripts/GUIBlink.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIBlink : MonoBehaviour
+{
+    public Animator blinkAnim;
+
+    void Start()
+    {
+        InvokeRepeating("Blink", 0f, 5f);
+    }
+
+    void Blink()
+    {
+        blinkAnim.SetTrigger("blink");
+    }
+
+}
