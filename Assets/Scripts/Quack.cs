2316bc11337ce3fc767b07fb962a2180303b27e1
Thor Bunting <thor.bunting@live.co.uk>
2017-01-22 11:27:37 +0000

    Quack

---
+++ Assets/Scripts/Quack.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quack : MonoBehaviour
+{
+    public AudioClip quack;
+    public AudioSource quackSource;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Tentacle")
+        {
+            quackSource.PlayOneShot(quack);
+        }
+    }
+
+}
